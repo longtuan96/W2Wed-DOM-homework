3b805ede677b30be01af4b3719c1c318e98ec108
Tuan <longtuan03@gmail.com>
2021-03-24 12:55:34 +0700

    detach

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,29 @@
-console.log('Hello JS!')
+console.log("Hello JS!");
 
-// document.getElementById('change-text').innerHTML = 'Hello Change!'
+window.onload = runAfterPageload;
+function runAfterPageload() {
+  after3Sec();
+  countAfterload();
+}
 
-const count = 0
+//Please change me after 3 sec
+// window.onload = after3Sec(); //run after the page load
+function after3Sec() {
+  setTimeout(changeHTML, 3000);
+}
+function changeHTML() {
+  document.getElementById("change-text").innerHTML = "Hello Change!";
+}
+
+//Increment the count in the next box every second
+let count = 0;
+
+// window.onload = countAfterload();
+function countAfterload() {
+  setInterval(counting(), 1000);
+}
+
+function counting() {
+  document.getElementById("change-count").innerHTML = count;
+  count++;
+}
